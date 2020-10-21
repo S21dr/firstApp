5f7569a448e3a2a324405a3d223af0dea559018c
S21dr <adel.ernandies@mail.ru>
2020-10-21 13:11:21 +0300

    Updates

---
+++ precache-manifest.897129295b97e3a07bd5b328f431fe13.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4669a600b7a4174cbd56044029618d07",
+    "url": "/firstApp/index.html"
+  },
+  {
+    "revision": "d0dec3b60854c038f63e",
+    "url": "/firstApp/static/css/main.b8adbc9c.chunk.css"
+  },
+  {
+    "revision": "ef03b3745bdd625f525f",
+    "url": "/firstApp/static/js/2.1d5ca552.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/firstApp/static/js/2.1d5ca552.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d0dec3b60854c038f63e",
+    "url": "/firstApp/static/js/main.b953262d.chunk.js"
+  },
+  {
+    "revision": "d824b1b0a61698cf2d81",
+    "url": "/firstApp/static/js/runtime-main.531e9fde.js"
+  },
+  {
+    "revision": "3551210e409b90428182b865f85a16df",
+    "url": "/firstApp/static/media/loader.3551210e.gif"
+  }
+]);
